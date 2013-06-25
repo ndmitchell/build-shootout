0e6c6e764ce07c1d18940997c7edc83a3302acd9
Neil Mitchell <ndmitchell@gmail.com>
2013-06-25 18:38:22 +0100

    Add a wildcard example

--- Main.hs
+++ Main.hs
@@ -11,6 +11,7 @@ main = do
     test "basic" basic
     test "parallel" parallel
     test "include" include
+    test "wildcard" wildcard
 
 
 basic :: ([Opt] -> IO ()) -> IO ()
@@ -38,3 +39,15 @@ include run = do
     touch "include-2.h"
     run [Change "main.o"]
     run [NoChange]
+
+
+wildcard :: ([Opt] -> IO ()) -> IO ()
+wildcard run = do
+    x <- randomRIO (1::Int,1000000)
+    let name = "name" ++ show x
+    writeFile (name ++ ".in") "abc"
+    run [Target $ name ++ ".out", Contents (name ++ ".out") "abc"]
+    run [Target $ name ++ ".out", NoChange]
+    writeFile (name ++ ".in") "xyz"
+    run [Target $ name ++ ".out", Contents (name ++ ".out") "xyz"]
+    run [Target $ name ++ ".out", NoChange]
---
+++ examples/wildcard-shake.hs
@@ -0,0 +1,6 @@
+import Development.Shake
+import Development.Shake.FilePath
+
+main = shakeArgs shakeOptions $ do
+    "*.out" *> \out -> do
+        copyFile' (out -<.> "in") out
