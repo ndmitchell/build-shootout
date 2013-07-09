3ccd6b7107999ff6580f666f6682843eee570d3c
Neil Mitchell <ndmitchell@gmail.com>
2013-07-09 20:42:09 +0100

    shake can implement monad3

---
+++ examples/monad3-shake.hs
@@ -0,0 +1,15 @@
+import Development.Shake
+import Development.Shake.FilePath
+
+main = shakeArgs shakeOptions $ do
+    "output" *> \out -> do
+        src <- readFileLines "list"
+        need src
+        cmd Shell "cat" src ">" [out]
+
+    "list" *> \out -> do
+        need ["source"]
+        cmd Shell "sh monad3-run source list"
+
+    "gen" *> \out -> do
+        cmd Shell "sh monad3-gen gen"
