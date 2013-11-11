663d2c4272b318b44754fdcf52aa6d902f0cb4b1
Neil Mitchell <ndmitchell@gmail.com>
2013-11-11 20:39:12 +0000

    Rewrite multiple shake to be the standard pattern

--- examples/multiple-shake.hs
+++ examples/multiple-shake.hs
@@ -1,10 +1,11 @@
 import Development.Shake
 
 main = shakeArgs shakeOptions $ do
-    "output*" *> \out -> do
-        let src = "source" ++ [last out]
-        need [src]
-        cmd "sh multiple-run" [src] " -- " [out]
+    let run inp out = out *> \_ -> do
+            need [inp]
+            cmd "sh multiple-run" [inp] "--" [out]
+    run "source1" "output1"
+    run "source2" "output2"
     ["source1","source2"] **> \out -> do
         need ["input"]
         cmd "sh multiple-gen input -- source1 source2"
