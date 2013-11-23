e595212b8ac2a1a1d34bb738b1b1bd88e4e2c853
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2013-11-23 09:43:43 +0100

    Make rules clean up after themselves

--- Main.hs
+++ Main.hs
@@ -5,6 +5,7 @@
 module Main(main) where
 
 import Util
+import System.Directory
 
 
 main :: IO ()
@@ -31,6 +32,8 @@ basic run = do
     writeFile "input" "abc"
     run [Contents "output" "abc"]
     run [NoChange]
+    removeFile "input"
+    removeFile "output"
 
 
 parallel :: ([Opt] -> IO ()) -> IO ()
@@ -39,6 +42,10 @@ parallel run = do
     writeFile "input2" "abc"
     run [Parallel 2, Contents ".log" "start\nstart\nend\nend\n"]
     run [NoChange]
+    removeFile "input1"
+    removeFile "input2"
+    removeFile "output1"
+    removeFile "output2"
 
 
 include :: ([Opt] -> IO ()) -> IO ()
@@ -104,6 +111,10 @@ monad2 run = do
     run [Target "output", NoChange]
     writeFile "input2" "x"
     run [Target "output", NoChange, Contents ".log" "run\nrun\n"]
+    removeFile "input1"
+    removeFile "input2"
+    removeFile "output"
+    removeFile "source"
 
 
 monad3 :: ([Opt] -> IO ()) -> IO ()
