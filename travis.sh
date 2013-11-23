2b2557dfa4af05b7213dc6aa090c622f4288415e
Neil Mitchell <ndmitchell@gmail.com>
2013-11-23 21:04:07 +0000

    Turn of tup support on the travis machine, FUSE doesn't work

--- travis.sh
+++ travis.sh
@@ -8,11 +8,12 @@ git clone https://github.com/martine/ninja
 export PATH=$PATH:`pwd`/ninja
 
 # Install tup
-sudo apt-get install libfuse-dev
-modprobe fuse
-git clone https://github.com/gittup/tup
-(cd tup && ./bootstrap.sh)
-export PATH=$PATH:`pwd`/tup
+# Does not work, see: https://github.com/travis-ci/travis-ci/issues/1100
+# sudo apt-get install libfuse-dev
+# modprobe fuse
+# git clone https://github.com/gittup/tup
+# (cd tup && ./bootstrap.sh)
+# export PATH=$PATH:`pwd`/tup
 
 # Run the tests
-runhaskell Main
+runhaskell Main shake make ninja
