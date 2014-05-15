75641811f81252f072843f6787db4336eecc7d74
Ryan Gonzalez <rymg19@gmail.com>
2014-05-15 17:04:58 -0500

    Fixed Fabricate wildcard example

--- examples/wildcard-fabricate.py
+++ examples/wildcard-fabricate.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 from fabricate import *
-import os, glob
+import sys, os, glob
 
-def build():
-    for f in glob.glob('*.in'):
-        run('cp', f, os.path.splitext(f)[0]+'.out')
+def get_cp(inf, out):
+    @staticmethod
+    def _f(): run('cp', inf, out)
+    return _f
+
+for f in glob.glob('*.in'):
+    base = os.path.splitext(f)[0]
+    globals()[base] = type(base, (object,), {'out': get_cp(f, base+'.out')})
+
+#sys.argv[1:] = [(arg[:-4]+'__DOT_OUT') if arg.endswith('.out') else arg\
+#    for arg in sys.argv[1:]]
 
 main()
