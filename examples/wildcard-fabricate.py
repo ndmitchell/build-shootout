1cc2f471dd6e935fa1c42ad9646bcaca5935c79b
Ryan Gonzalez <rymg19@gmail.com>
2014-05-16 13:43:20 -0500

    Removed commented out code

--- examples/wildcard-fabricate.py
+++ examples/wildcard-fabricate.py
@@ -11,7 +11,4 @@ for f in glob.glob('*.in'):
     base = os.path.splitext(f)[0]
     globals()[base] = type(base, (object,), {'out': get_cp(f, base+'.out')})
 
-#sys.argv[1:] = [(arg[:-4]+'__DOT_OUT') if arg.endswith('.out') else arg\
-#    for arg in sys.argv[1:]]
-
 main()
